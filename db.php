401b7bf78c40cda36e3c6a4490f43d7702a11597
Joaquin Bocanegra <104942320+JoaquinBocanegra@users.noreply.github.com>
2022-08-24 20:45:44 -0300

    Add files via upload

--- db.php
+++ db.php
@@ -1,8 +1,19 @@
 <?php
-
+/** Clase para manejar base de datos, usando PDO.
+ * 
+ * @author Joaquin Bocanegra <joaquinbocanegra@email.com>
+ * @link https://github.com/JoaquinBocanegra/
+ * @version 1.0.0
+ * @copyright 2022 Curso programador de aplicaciones web.
+ * 
+ */
 class Db
 {
 private $connection;
+/** Establecer una conexión.
+ * 
+ * @return connection.
+ */
 public function __construct()
 {
     try{
@@ -15,6 +26,30 @@ public function __construct()
         die('Conexión fallida ');
     }
     return $this->connection;
+}
+/** Preparar una query SQL.
+ * 
+ * @return object
+ */
+public function prepare($query){
+    return $this->connection->prepare($query);
+
+}
+/** Ejecutar una SQL query.
+ * 
+ *
+ * @param string $query.
+ * @return object 
+ */
+public function query($query){
+    return $this->connection->query($query);
+}
+/** Cerrar la conexión.
+ * 
+ * @return mixed
+ */
+public function close(){
+    $this->connection=null;
 }
 }
 
